dab20b36d2b49038348fd3999155c90736be9916
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2024-11-13 07:09:03 +0100

    quest 1

---
+++ README.md
@@ -0,0 +1,4 @@
+My solutions to Everybody Codes Kingdom of Algorithmia in Rust.
+
+Run a quest with (put input in "/input" at root with name "questX_p1.txt", "questX_p2.txt" and "questX_p3.txt")
+> cargo run --bin questX
---
+++ src/bin/quest1.rs
@@ -0,0 +1,50 @@
+use std::fs::read_to_string;
+
+fn main() {
+	let file = read_to_string("input/quest1_p1.txt").unwrap();
+
+	let part1 = file.chars().into_iter().map(|x| 
+		if x == 'A' { 0 } else if x == 'B' { 1 } else { 3 }
+	).sum::<i32>();
+
+	println!("Part 1: {}", part1);
+
+	let file = read_to_string("input/quest1_p2.txt").unwrap();
+
+	let vec = file.chars().into_iter().collect::<Vec<char>>();
+
+	let part2 = vec.as_slice().chunks(2).map(|xs| {
+		let x = xs[0];
+		let y = xs[1];
+		let sum = num(x) + num(y);
+		return if x != 'x' && y != 'x' { sum + 2 } else { sum }
+	}).sum::<usize>();
+
+	println!("Part 2: {}", part2);
+
+	let file = read_to_string("input/quest1_p3.txt").unwrap();
+
+	let vec = file.chars().into_iter().collect::<Vec<char>>();
+
+	let part2 = vec.as_slice().chunks(3).map(|xs| {
+		let x = xs[0];
+		let y = xs[1];
+		let z = xs[2];
+		let sum = num(x) + num(y) + num(z);
+		let xx = xs.iter().filter(|&&x| x == 'x').count();
+		println!("{}, {}", sum, xx);
+		if xx == 0 {
+			return sum + 6;
+		}
+		if xx == 1 {
+			return sum + 2;
+		}
+		sum
+	}).sum::<usize>();
+
+	println!("Part 3: {}", part2);
+}
+
+fn num(x: char) -> usize {
+	if x == 'B' { 1 } else if x == 'C' { 3 } else if x == 'D' { 5 } else { 0 }
+}
