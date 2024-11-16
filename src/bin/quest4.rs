3f7f9b65cc13252c16d5edb8cd412a82067a9bde
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2024-11-16 14:30:15 +0100

    quest 4

---
+++ src/bin/quest4.rs
@@ -0,0 +1,52 @@
+use std::{collections::{HashMap, HashSet}, fs::read_to_string, usize};
+
+fn main() {
+	part1();
+	part2();
+	part3();
+}
+
+fn part1() {
+	let file = read_to_string("input/quest4_p1.txt").unwrap();
+
+	let res = solve1a2(file);
+
+	println!("Part 1: {:?}", res);
+}
+
+fn part2() {
+	let file = read_to_string("input/quest4_p2.txt").unwrap();
+
+	let res = solve1a2(file);
+
+	println!("Part 1: {:?}", res);
+}
+
+fn solve1a2(file: String) -> usize {
+	let nums = file.lines().map(|x| x.parse::<usize>().unwrap()).collect::<Vec<_>>();
+
+	let min = *nums.iter().min().unwrap();
+
+	nums.iter().map(|x| x - min).sum::<usize>()
+}
+
+fn part3() {
+	let file = read_to_string("input/quest4_p3.txt").unwrap();
+
+	let nums = file.lines().map(|x| x.parse::<usize>().unwrap()).collect::<Vec<_>>();
+
+	let sum = nums.iter().sum::<usize>();
+	let mut avg = sum / nums.len();
+	let mut res = usize::MAX;
+	loop {
+		let t = nums.iter().map(|x| x.abs_diff(avg)).sum::<usize>();
+
+		if t > res {
+			break;
+		}
+		res = t;
+		avg += 1;
+	}
+
+	println!("Part 1: {:?}", res);
+}
