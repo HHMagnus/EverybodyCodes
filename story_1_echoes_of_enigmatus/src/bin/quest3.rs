99cfca065dd03927114174ae91f21e53e44a538b
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2025-10-31 19:44:03 +0100

    part 2

--- story_1_echoes_of_enigmatus/src/bin/quest3.rs
+++ story_1_echoes_of_enigmatus/src/bin/quest3.rs
@@ -1,14 +1,10 @@
+use std::cmp::min;
 use std::fs::read_to_string;
+use std::mem::swap;
 
 fn main() {
     let file = read_to_string("input/quest3/input1.txt").unwrap();
-    let coords = file.lines()
-        .map(|line| {
-            let mut split = line.split(" ");
-            let x = split.next().unwrap().replace("x=", "").parse::<i32>().unwrap();
-            let y = split.next().unwrap().replace("y=", "").parse::<i32>().unwrap();
-            (x, y)
-        }).collect::<Vec<_>>();
+    let coords = parse(file);
 
     let part1 = coords.into_iter()
         .map(|coord| {
@@ -18,12 +14,30 @@ fn main() {
             }
             c
         })
-        .collect::<Vec<_>>();
-    println!("{:?}", part1);
-    let part1 = part1.into_iter()
         .map(|x: (i32, i32)| snail_position_formula(x))
         .sum::<i32>();
-    println!("{:?}", part1);
+    println!("Part 1: {:?}", part1);
+
+    let file = read_to_string("input/quest3/input2.txt").unwrap();
+    let mut coords = parse(file);
+    let mut part2 = 0;
+    while !coords.iter().all(|coord| coord.1 == 1) {
+        coords = coords.into_iter()
+            .map(diagonal)
+            .collect();
+        part2 += 1;
+    }
+    println!("Part 2: {:?}", part2);
+}
+
+fn parse(file: String) -> Vec<(i32, i32)> {
+    file.lines()
+        .map(|line| {
+            let mut split = line.split(" ");
+            let x = split.next().unwrap().replace("x=", "").parse::<i32>().unwrap();
+            let y = split.next().unwrap().replace("y=", "").parse::<i32>().unwrap();
+            (x, y)
+        }).collect::<Vec<_>>()
 }
 
 fn max((x, y): (i32, i32)) -> i32 {
