5ba7b4991ae0705f7a6377480e14fe23c9575e0c
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2025-10-31 15:17:36 +0100

    quest 3 part 1

--- story_1_echoes_of_enigmatus/src/bin/quest3.rs
+++ story_1_echoes_of_enigmatus/src/bin/quest3.rs
@@ -5,9 +5,44 @@ fn main() {
     let coords = file.lines()
         .map(|line| {
             let mut split = line.split(" ");
-            let x = split.next().unwrap().replace("x=", "").parse::<u32>().unwrap();
-            let y = split.next().unwrap().replace("y=", "").parse::<u32>().unwrap();
+            let x = split.next().unwrap().replace("x=", "").parse::<i32>().unwrap();
+            let y = split.next().unwrap().replace("y=", "").parse::<i32>().unwrap();
             (x, y)
-        }).collect::<Vec<(u32, u32)>>();
-    println!("{:?}", coords);
+        }).collect::<Vec<_>>();
+
+    let part1 = coords.into_iter()
+        .map(|coord| {
+            let mut c = coord;
+            for _ in 0..100 {
+                c = diagonal(c);
+            }
+            c
+        })
+        .collect::<Vec<_>>();
+    println!("{:?}", part1);
+    let part1 = part1.into_iter()
+        .map(|x: (i32, i32)| snail_position_formula(x))
+        .sum::<i32>();
+    println!("{:?}", part1);
+}
+
+fn max((x, y): (i32, i32)) -> i32 {
+    x + y - 1
+}
+
+fn diagonal((x, y): (i32, i32)) -> (i32, i32) {
+    let max = max((x, y));
+    let mut a = x + 1;
+    if a > max {
+        a = 1;
+    }
+    let mut b = y - 1;
+    if b <= 0 {
+        b = max;
+    }
+    (a, b)
+}
+
+fn snail_position_formula((x, y): (i32, i32)) -> i32 {
+    x + (100 * y)
 }
