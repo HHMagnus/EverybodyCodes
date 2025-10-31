7f6ef21b26b57cbbc883c5414cfa6ff541f33136
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2025-10-31 15:05:02 +0100

    quest 3 part 1 parse

--- story_1_echoes_of_enigmatus/src/bin/quest3.rs
+++ story_1_echoes_of_enigmatus/src/bin/quest3.rs
@@ -1,3 +1,13 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("test");
+    let file = read_to_string("input/quest3/input1.txt").unwrap();
+    let coords = file.lines()
+        .map(|line| {
+            let mut split = line.split(" ");
+            let x = split.next().unwrap().replace("x=", "").parse::<u32>().unwrap();
+            let y = split.next().unwrap().replace("y=", "").parse::<u32>().unwrap();
+            (x, y)
+        }).collect::<Vec<(u32, u32)>>();
+    println!("{:?}", coords);
 }
