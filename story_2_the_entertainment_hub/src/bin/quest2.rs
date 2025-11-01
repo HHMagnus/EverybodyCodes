cfc4cc3e6482e1b857bb5ac713481f0caf870345
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2025-11-01 15:25:58 +0100

    quest 2 part 1

--- story_2_the_entertainment_hub/src/bin/quest2.rs
+++ story_2_the_entertainment_hub/src/bin/quest2.rs
@@ -1,6 +1,39 @@
+use std::cmp::PartialEq;
 use std::fs::read_to_string;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum Balloon {
+    R,
+    G,
+    B
+}
 
 fn main() {
     let file = read_to_string("input/quest2/input1.txt").unwrap();
-    println!("test");
+
+    let mut balloons = file.chars()
+        .map(|c| match c {
+            'R' => Balloon::R,
+            'G' => Balloon::G,
+            'B' => Balloon::B,
+            _ => panic!("No balloon"),
+        })
+        .collect::<Vec<Balloon>>();
+    balloons.reverse();
+
+    let mut cycle = vec![Balloon::R, Balloon::G, Balloon::B].into_iter().cycle();
+
+    let mut part1 = 0;
+
+    while !balloons.is_empty() {
+        part1 += 1;
+
+        let fluffbolt = cycle.next().unwrap();
+
+        while let (Some(b)) = balloons.pop() && b == fluffbolt {
+
+        }
+    }
+
+    println!("Part 1: {}", part1);
 }
