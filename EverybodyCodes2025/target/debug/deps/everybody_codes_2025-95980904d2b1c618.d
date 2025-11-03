5a1d74140ff55b818588e6381dd2311469f2cc9a
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2025-11-03 20:32:31 +0100

    renamed 2025 event

--- EverybodyCodes2025/target/debug/deps/everybody_codes_2025-95980904d2b1c618.d
+++
@@ -1,5 +0,0 @@
-/Users/magnus/Documents/GitHub/EverybodyCodes2024/EverybodyCodes2025/target/debug/deps/everybody_codes_2025-95980904d2b1c618.d: src/main.rs
-
-/Users/magnus/Documents/GitHub/EverybodyCodes2024/EverybodyCodes2025/target/debug/deps/everybody_codes_2025-95980904d2b1c618: src/main.rs
-
-src/main.rs:
