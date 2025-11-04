31073a6bb25b53bc3407bbc7236da054cce13dff
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2025-11-04 16:49:40 +0100

    right input file

--- event_2025_the_song_of_ducks_and_dragons/src/bin/day2.rs
+++ event_2025_the_song_of_ducks_and_dragons/src/bin/day2.rs
@@ -1,6 +1,6 @@
 use std::fs::read_to_string;
 
 fn main() {
-    let file = read_to_string("input/quest1/input1.txt").unwrap();
+    let file = read_to_string("input/quest2/input1.txt").unwrap();
     println!("Day 2 {}", file);
 }
