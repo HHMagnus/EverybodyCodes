0e2bf6ceab8f3665437ad54d940ffadb40cc94e5
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2025-11-07 10:29:51 +0100

    day 2 part 1

--- event_2025_the_song_of_ducks_and_dragons/src/bin/day2.rs
+++ event_2025_the_song_of_ducks_and_dragons/src/bin/day2.rs
@@ -2,5 +2,33 @@ use std::fs::read_to_string;
 
 fn main() {
     let file = read_to_string("input/quest2/input1.txt").unwrap();
-    println!("Day 2 {}", file);
+    let parsed = file.replace("A=[", "")
+        .replace("]", "")
+        .split(",")
+        .map(|x| x.parse::<i64>().unwrap())
+        .collect::<Vec<_>>();
+    let input = (parsed[0], parsed[1]);
+
+    let mut value = (0, 0);
+    for _ in 0..3 {
+        value = cycle(value, input);
+        println!("{:?}, {:?}", value, value);
+    }
+    
+    println!("Part 1: [{},{}]", value.0, value.1);
+}
+
+fn cycle(xy: (i64, i64), a: (i64, i64)) -> (i64, i64) {
+    let (mut x, mut y) = xy;
+    let (ax, ay) = a;
+    // X1 * X2 - Y1 * Y2
+    let new_x = x * x - y * y;
+    // X1 * Y2 + Y1 * X2
+    y = x * y + y * x;
+    x = new_x;
+    x /= 10;
+    y /= 10;
+    x += ax;
+    y += ay;
+    (x, y)
 }
