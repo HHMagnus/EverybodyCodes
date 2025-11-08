953a50c93a748bfadc8c01432931a033157dcf7a
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2025-11-08 22:27:28 +0100

    day 2 part 2

--- event_2025_the_song_of_ducks_and_dragons/src/bin/day2.rs
+++ event_2025_the_song_of_ducks_and_dragons/src/bin/day2.rs
@@ -2,23 +2,45 @@ use std::fs::read_to_string;
 
 fn main() {
     let file = read_to_string("input/quest2/input1.txt").unwrap();
+    let input = parse(file);
+
+    let mut part1 = (0, 0);
+    for _ in 0..3 {
+        part1 = cycle(part1, input, 10);
+    }
+
+    println!("Part 1: [{},{}]", part1.0, part1.1);
+
+    let file = read_to_string("input/quest2/input2.txt").unwrap();
+    let input = parse(file);
+
+    let mut part2 = 0;
+    for y in (input.1..=input.1+1000).step_by(10) {
+        for x in (input.0..=input.0+1000).step_by(10) {
+            let point = (x, y);
+            if cycle100(point, 100000) {
+                part2 += 1;
+                print!("X");
+            } else {
+                print!(".");
+            }
+        }
+        print!("\n");
+    }
+    println!("Part 2: {}", part2);
+}
+
+fn parse(file: String) -> (i64, i64) {
     let parsed = file.replace("A=[", "")
         .replace("]", "")
         .split(",")
         .map(|x| x.parse::<i64>().unwrap())
         .collect::<Vec<_>>();
     let input = (parsed[0], parsed[1]);
-
-    let mut value = (0, 0);
-    for _ in 0..3 {
-        value = cycle(value, input);
-        println!("{:?}, {:?}", value, value);
-    }
-    
-    println!("Part 1: [{},{}]", value.0, value.1);
+    input
 }
 
-fn cycle(xy: (i64, i64), a: (i64, i64)) -> (i64, i64) {
+fn cycle(xy: (i64, i64), a: (i64, i64), division: i64) -> (i64, i64) {
     let (mut x, mut y) = xy;
     let (ax, ay) = a;
     // X1 * X2 - Y1 * Y2
@@ -26,9 +48,20 @@ fn cycle(xy: (i64, i64), a: (i64, i64)) -> (i64, i64) {
     // X1 * Y2 + Y1 * X2
     y = x * y + y * x;
     x = new_x;
-    x /= 10;
-    y /= 10;
+    x /= division;
+    y /= division;
     x += ax;
     y += ay;
     (x, y)
+}
+
+fn cycle100(xy: (i64, i64), division: i64) -> bool {
+    let mut curr = xy;
+    for _ in 0..99 {
+        curr = cycle(curr, xy, division);
+        if curr.0 > 1000000 || curr.1 > 1000000 || curr.0 < -1000000 || curr.1 < -1000000 {
+            return false;
+        }
+    }
+    true
 }
