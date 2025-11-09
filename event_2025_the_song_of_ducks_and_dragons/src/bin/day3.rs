90eb106466e32109af2a1a4c08483dad9cf7ea71
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2025-11-09 20:59:27 +0100

    quest 3 done

--- event_2025_the_song_of_ducks_and_dragons/src/bin/day3.rs
+++ event_2025_the_song_of_ducks_and_dragons/src/bin/day3.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use event_2025_the_song_of_ducks_and_dragons::solve;
 
 fn main() {
@@ -5,13 +6,33 @@ fn main() {
 }
 
 fn part1(file: &str) -> i32 {
-    0
+    let nums = parse(file)
+        .into_iter()
+        .collect::<HashSet<_>>();
+    nums.iter().sum()
+}
+
+fn parse(file: &str) -> Vec<i32> {
+    file.split(",")
+        .map(|num| num.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>()
 }
 
 fn part2(file: &str) -> i32 {
-    0
+    let mut nums = parse(file);
+    nums.sort();
+    nums.iter()
+        .take(20)
+        .sum()
 }
 
-fn part3(file: &str) -> i32 {
-    0
+fn part3(file: &str) -> usize {
+    let mut nums = parse(file);
+    nums.sort();
+    nums.as_slice()
+        .chunk_by(|x, y| x == y)
+        .into_iter()
+        .map(|v| v.len())
+        .max()
+        .unwrap()
 }
