9b073949cba4861b935c5b57625a098d1265ad52
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2025-11-09 14:20:53 +0100

    refactored day 1

--- event_2025_the_song_of_ducks_and_dragons/src/bin/day1.rs
+++ event_2025_the_song_of_ducks_and_dragons/src/bin/day1.rs
@@ -1,5 +1,3 @@
-use std::fmt::Display;
-use std::fs::read_to_string;
 use event_2025_the_song_of_ducks_and_dragons::solve;
 
 enum Direction {
@@ -8,17 +6,48 @@ enum Direction {
 }
 
 fn main() {
-    let file = read_to_string("input/quest1/input1.txt").unwrap();
-    solve("Part 1", &file, part1);
-
-    let file = read_to_string("input/quest1/input2.txt").unwrap();
-    solve("Part 2", &file, part2);
-
-    let file = read_to_string("input/quest1/input3.txt").unwrap();
-    solve("Part 3", &file, part3);
+    solve("1", part1, part2, part3);
 }
 
-fn part3(file: &str) -> impl Display{
+fn part1(file: &str) -> String {
+    let (names, instructions) = parse(&file);
+
+    let mut curr = 0;
+    for instruction in instructions {
+        match instruction {
+            Direction::Right(n) => curr += n,
+            Direction::Left(n) => curr -= n
+        }
+        if curr < 0 {
+            curr = 0;
+        }
+        if curr >= names.len() as i32 {
+            curr = names.len() as i32 - 1;
+        }
+    }
+    names[curr as usize].to_string()
+}
+
+fn part2(file: &str) -> String {
+    let (names, instructions) = parse(&file);
+
+    let mut curr = 0;
+    for instruction in instructions {
+        match instruction {
+            Direction::Right(n) => curr += n,
+            Direction::Left(n) => curr -= n
+        }
+        while curr < 0 {
+            curr += names.len() as i32;
+        }
+        while curr >= names.len() as i32 {
+            curr -= names.len() as i32;
+        }
+    }
+    names[curr as usize].to_string()
+}
+
+fn part3(file: &str) -> String {
     let (names, instructions) = parse(&file);
 
     let mut names = names;
@@ -38,45 +67,7 @@ fn part3(file: &str) -> impl Display{
         names[0] = names[replace as usize];
         names[replace as usize] = temp;
     }
-    names[0]
-}
-
-fn part2(file: &str) -> impl Display {
-    let (names, instructions) = parse(&file);
-
-    let mut curr = 0;
-    for instruction in instructions {
-        match instruction {
-            Direction::Right(n) => curr += n,
-            Direction::Left(n) => curr -= n
-        }
-        while curr < 0 {
-            curr += names.len() as i32;
-        }
-        while curr >= names.len() as i32 {
-            curr -= names.len() as i32;
-        }
-    }
-    names[curr as usize]
-}
-
-fn part1(file: &str) -> impl Display{
-    let (names, instructions) = parse(&file);
-
-    let mut curr = 0;
-    for instruction in instructions {
-        match instruction {
-            Direction::Right(n) => curr += n,
-            Direction::Left(n) => curr -= n
-        }
-        if curr < 0 {
-            curr = 0;
-        }
-        if curr >= names.len() as i32 {
-            curr = names.len() as i32 - 1;
-        }
-    }
-    names[curr as usize]
+    names[0].to_string()
 }
 
 fn parse(file: &str) -> (Vec<&str>, Vec<Direction>) {
--- event_2025_the_song_of_ducks_and_dragons/src/lib.rs
+++ event_2025_the_song_of_ducks_and_dragons/src/lib.rs
@@ -1,9 +1,37 @@
 use std::fmt::Display;
+use std::fs::read_to_string;
 
-pub fn solve<'a, F, R>(puzzle: &'a str, file: &'a str, solver: F)
-where F: Fn(&'a str) -> R,
+fn solve_part<F, R>(puzzle: String, file: &str, solver: F)
+where F: Fn(&str) -> R,
       R: Display
 {
     let result = solver(file);
     println!("{}: {}", puzzle, result);
+}
+
+pub fn solve<F1, F2, F3, R1, R2, R3>(puzzle: &str, part1: F1, part2: F2, part3: F3)
+where F1: Fn(&str) -> R1,
+      R1: Display,
+      F2: Fn(&str) -> R2,
+      R2: Display,
+      F3: Fn(&str) -> R3,
+      R3: Display,
+{
+    run_part(puzzle, part1, "1");
+    run_part(puzzle, part2, "2");
+    run_part(puzzle, part3, "3");
+}
+
+fn run_part<F, R>(puzzle: &str, part1: F, input: &str)
+where
+    F: Fn(&str) -> R,
+    R: Display
+{
+    let title = format!("Quest {} part {}", puzzle, input);
+    let file = read_to_string(format!("input/quest{}/input{}.txt", puzzle, input));
+    if let Ok(file) = file {
+        solve_part(title, &file, part1);
+    } else {
+        println!("{}: Input missing", title);
+    }
 }
