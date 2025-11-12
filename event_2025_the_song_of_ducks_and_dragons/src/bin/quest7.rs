2291e422471a342ee2105937b042b0e07d4fed41
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2025-11-12 19:02:19 +0100

    quest 7 prep

---
+++ event_2025_the_song_of_ducks_and_dragons/src/bin/quest7.rs
@@ -0,0 +1,17 @@
+use event_2025_the_song_of_ducks_and_dragons::solve;
+
+fn main() {
+    solve("7", part1, part2, part3);
+}
+
+fn part1(file: &str) -> usize {
+    0
+}
+
+fn part2(file: &str) -> usize {
+    0
+}
+
+fn part3(file: &str) -> usize {
+    0
+}
