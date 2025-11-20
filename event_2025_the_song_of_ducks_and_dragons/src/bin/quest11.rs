8e711371a7e592e9a7c6457b24b7485a2b9cb0e4
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2025-11-20 19:43:41 +0100

    quest 11 part 3 wip

--- event_2025_the_song_of_ducks_and_dragons/src/bin/quest11.rs
+++ event_2025_the_song_of_ducks_and_dragons/src/bin/quest11.rs
@@ -1,3 +1,4 @@
+use std::cmp::min;
 use event_2025_the_song_of_ducks_and_dragons::solve;
 
 fn main() {
@@ -95,6 +96,61 @@ fn part2(file: &str) -> usize {
     total
 }
 
-fn part3(file: &str) -> usize {
-    0
+fn part3(file: &str) -> i64 {
+    let mut nums = file.split('\n')
+        .map(|n| n.parse::<i64>().unwrap())
+        .collect::<Vec<_>>();
+
+    let mut total = 0;
+
+    loop {
+        let mut moved = false;
+
+        let amount = nums.windows(2)
+            .map(|w| w[1] - w[0])
+            .filter(|n| *n > 0)
+            .min()
+            .unwrap()
+            .min(1i64);
+
+        for i in 1..nums.len() {
+            if nums[i - 1] > nums[i] {
+                nums[i - 1] -= amount;
+                nums[i] += amount;
+                moved = true;
+            }
+        }
+
+        if !moved {
+            break;
+        }
+
+        total += amount;
+    }
+
+    loop {
+        let mut moved = false;
+
+        let amount = nums.windows(2)
+            .map(|w| w[1] - w[0])
+            .filter(|n| *n > 0)
+            .min()
+            .unwrap_or(1i64)
+            .min(1i64);
+
+        for i in 1..nums.len() {
+            if nums[i - 1] < nums[i] {
+                nums[i - 1] += amount;
+                nums[i] -= amount;
+                moved = true;
+            }
+        }
+
+        if !moved {
+            break;
+        }
+        total += amount;
+    }
+
+    total
 }
